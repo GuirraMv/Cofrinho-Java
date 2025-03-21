c4be13b9028f12e76d3d3d031231c200702a4e4b
GuirraMv <matheusguirravieira15@gmail.com>
2025-03-21 20:29:01 -0300

    feat: improving methods

--- src/Cofrinho.java
+++ src/Cofrinho.java
@@ -1,25 +1,24 @@
 import java.util.ArrayList;
 
 public class Cofrinho {
+    int nomeMoeda;
     private ArrayList<Moeda> listaMoedas = new ArrayList<Moeda>();
 
     public void adiconar(Moeda moeda) {
         listaMoedas.add(moeda);
-        System.out.println(moeda.valor);
-        System.out.println(listaMoedas.get(listaMoedas.size() - 1).valor);
     }
-
-    ;
 
     public void remover(Moeda moeda) {
         listaMoedas.remove(moeda);
     }
 
-    ;
-
     public void listagemMoedas() {
+        for (Moeda moeda : listaMoedas) {
+            System.out.println(moeda.nome + " " + moeda.valor);
+        }
     }
 
-    ;
-//    public float totalConvertido(){};
+    public float totalConvertido() {
+        return 1;
+    }
 }
