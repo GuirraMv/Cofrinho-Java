8e23b4d11431df994d54a18c3eb31e6bd333e580
GuirraMv <matheusguirravieira15@gmail.com>
2025-03-24 23:03:06 -0300

    feat: adding equals method

--- src/Euro.java
+++ src/Euro.java
@@ -11,6 +11,16 @@ public class Euro extends Moeda {
     public void info() {
     }
 
+    @Override
+    public boolean equals(Object objeto) {
+        // Verifica se é o mesmo objeto na memória
+        if (this == objeto) return true;
+        // Garante que o objeto não é nulo e é do mesmo tipo
+        if (objeto == null || getClass() != objeto.getClass()) return false;
+        Euro other = (Euro) objeto;
+        return Double.compare(valor, other.valor) == 0;
+    }
+
     public double converter(double valor) {
         this.valor = valor;
         return super.converter(cotacao, valor);
--- src/Real.java
+++ src/Real.java
@@ -11,6 +11,16 @@ public class Real extends Moeda {
     public void info() {
     }
 
+    @Override
+    public boolean equals(Object objeto) {
+        // Verifica se é o mesmo objeto na memória
+        if (this == objeto) return true;
+        // Garante que o objeto não é nulo e é do mesmo tipo
+        if (objeto == null || getClass() != objeto.getClass()) return false;
+        Real other = (Real) objeto;
+        return Double.compare(valor, other.valor) == 0;
+    }
+
     public double converter(double valor) {
         this.valor = valor;
         return super.converter(cotacao, valor);
