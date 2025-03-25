b6a27bce5d7e6b7dd2a81bb2e9875060f1cdcf44
GuirraMv <matheusguirravieira15@gmail.com>
2025-03-24 23:03:44 -0300

    feat: creating remove method

--- src/Cofrinho.java
+++ src/Cofrinho.java
@@ -1,19 +1,36 @@
 import java.util.ArrayList;
+import java.util.Objects;
 
 public class Cofrinho {
     private ArrayList<Moeda> listaMoedas = new ArrayList<Moeda>();
 
-    public void adiconar(Moeda moeda) {
+    public void adicionar(Moeda moeda) {
         listaMoedas.add(moeda);
     }
 
     public void remover(Moeda moeda) {
-        listaMoedas.remove(moeda);
+        // verifica se a moeda inserida existe no array
+        if (listaMoedas.contains(moeda)) {
+            // verifica se o valor da moeda inserida é igual à moeda armazenada
+            if (moeda.valor == listaMoedas.get(listaMoedas.size() - 1).valor) {
+                listaMoedas.remove(listaMoedas.size() - 1);
+                System.out.println(moeda.nome + " " + moeda.valor + " removido com sucesso.");
+            } else {
+                System.out.println("Não existe " + moeda.nome + " armazenado no valor de " + moeda.valor);
+            }
+        } else {
+            System.out.println("Moeda não encontrada. Selecione uma moeda existente para remover.");
+        }
     }
 
     public void listagemMoedas() {
-        for (Moeda moeda : listaMoedas) {
-            System.out.println(moeda.nome + " " + moeda.valor);
+        // se listaMoedas não estiver vazia irá exibir todos os elementos pelo for
+        if (!listaMoedas.isEmpty()) {
+            for (Moeda moeda : listaMoedas) {
+                System.out.println(moeda.nome + " " + moeda.valor);
+            }
+        } else {
+            System.out.println("Não há moedas no cofrinho! Adicione uma moeda para o cofrinho.");
         }
     }
 
@@ -23,11 +40,12 @@ public class Cofrinho {
         double realConvertido = 0;
         double totalConvertido;
 
+        // para cada moeda armazenada confere se o nome é igual e faz a conversão conforme definido na classe
         for (Moeda moeda : listaMoedas) {
-            if (moeda.nome == "Dolar") {
+            if (Objects.equals(moeda.nome, "Dolar")) {
                 Dolar dolar = new Dolar(moeda.valor);
                 dolarConvertido = dolar.converter(moeda.valor);
-            } else if (moeda.nome == "Euro") {
+            } else if (Objects.equals(moeda.nome, "Euro")) {
                 Euro euro = new Euro(moeda.valor);
                 euroConvertido = euro.converter(moeda.valor);
             } else {
@@ -37,7 +55,7 @@ public class Cofrinho {
         }
 
         totalConvertido = (dolarConvertido + euroConvertido + realConvertido);
-        System.out.println("O total convertido em reais: " + totalConvertido);
+        System.out.println("O total convertido em Reais é de R$: " + totalConvertido);
         return totalConvertido;
     }
 }
